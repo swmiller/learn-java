43d61caeb8d1be7de82f9e5f8d18ca80dfac658e
Stephen Miller <smiller@crowninformationservices.com>
2025-03-12 14:04:02 -0500

    First program commit

---
+++ my-code/section-1/Workbook 1.1/FirstProgram.java
@@ -0,0 +1,9 @@
+// I'm a comment. I have no impact on your code.
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Printing is fun!");
+        System.out.println("Java > Python");
+        System.out.println("I spill Java all over my paper.");
+        System.out.println("My dog at my Java.");
+    }
+}
