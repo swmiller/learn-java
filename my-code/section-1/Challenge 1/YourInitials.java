ac0c15c768b8706cb7009e6a763079f814015888
Stephen Miller <smiller@crowninformationservices.com>
2025-03-12 14:13:02 -0500

    First challenge

---
+++ my-code/section-1/Challenge 1/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println(" SSS   M   M");
+        System.out.println("S   S  MM MM");
+        System.out.println("S      M M M");
+        System.out.println(" SSS   M   M");
+        System.out.println("    S  M   M");
+        System.out.println("S   S  M   M");
+        System.out.println(" SSS   M   M");
+    }
+}
