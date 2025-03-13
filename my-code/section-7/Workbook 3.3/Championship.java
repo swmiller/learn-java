b7845240d73c88ecc4224e36ee2f570ce2e5a6ca
Stephen Miller <smiller@crowninformationservices.com>
2025-03-13 13:09:37 -0500

    Finished workbook 3.3

--- my-code/section-7/Workbook 3.3/Championship.java
+++ my-code/section-7/Workbook 3.3/Championship.java
@@ -1,12 +1,23 @@
 public class Championship {
     public static void main(String[] args) {
 
-        //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
+        // See Learn the Part for the complete instructions (link in resources folder of
+        // Udemy video).
 
-        int gryffindor = 400;    //gryffindor points
-        int ravenclaw = 200;    //ravenclaw points
+        int gryffindor = 100; // gryffindor points
+        int ravenclaw = 500; // ravenclaw points
 
         // int margin = amount of points by which gryffindor scored over ravenclaw;
-        
+        int margin = gryffindor - ravenclaw;
+
+        if (margin >= 300) {
+            System.out.println("Gryffindor wins the cup!");
+        } else if (margin >= 0) {
+            System.out.println("In second place: Gryffindor");
+        } else if (margin >= -100) {
+            System.out.println("In third place: Gryffindor");
+        } else {
+            System.out.println("In fourth place: Gryffindor");
+        }
     }
 }
