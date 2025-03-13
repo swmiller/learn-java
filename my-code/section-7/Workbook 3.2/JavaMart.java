c344597f85e597f365f547ba90388cee9118a20e
Stephen Miller <smiller@crowninformationservices.com>
2025-03-13 13:01:55 -0500

    Finished workbook 3.2

--- my-code/section-7/Workbook 3.2/JavaMart.java
+++ my-code/section-7/Workbook 3.2/JavaMart.java
@@ -2,26 +2,27 @@ public class JavaMart {
     public static void main(String[] args) {
         double wallet = 100;
 
-        //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
+        // See Learn the Part for the complete instructions (link in resources folder of
+        // Udemy video).
 
- 
         double toyCar = 5.99;
         System.out.println("Can I get this car?");
-       
-        //if you have enough money
-            //       print: Sure!
-            //       pay for the toy car 
+        if (wallet >= toyCar) {
+            System.out.println("Sure!");
+            wallet -= toyCar;
+            System.out.println("You have " + wallet + " left.");
+        } else {
+            System.out.println("Sorry, I only have " + wallet + " left.");
+        }
 
-        //else:  Sorry, I only have <wallet> left.
-
-        
         double nike = 95.99;
         System.out.println("Can I get these nike shoes?");
-        //if you have enough money
-            //       print: Sure!
-            //       pay for the nike shoes 
-
-        //else: Sorry, I only have <wallet> left.
-
+        if (wallet >= nike) {
+            System.out.println("Sure!");
+            wallet -= nike;
+            System.out.println("You have " + wallet + " left.");
+        } else {
+            System.out.println("Sorry, I only have " + wallet + " left.");
+        }
     }
 }
