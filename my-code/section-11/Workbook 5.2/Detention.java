80aaa332459cb098606d3a59b233534ce61c75c1
Stephen Miller <smiller@crowninformationservices.com>
2025-03-14 09:58:47 -0500

    Finished workbook 5.2

--- my-code/section-11/Workbook 5.2/Detention.java
+++ my-code/section-11/Workbook 5.2/Detention.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Detention {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
         System.out.println("Hi Bart. I can write lines for you.\nWhat do you want me to write?");
         // See Learn the Part for the instructions.
-        
+        String bartsInput = input.nextLine();
+
+        System.out.println("\nHere you go ...");
+        for (int i = 0; i < 99; i++) {
+            System.out.println(bartsInput);
+        }
+
+        input.close();
     }
 }
