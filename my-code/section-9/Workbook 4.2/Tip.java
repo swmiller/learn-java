5b01ad785eed845126236d79ef37984a3891c998
Stephen Miller <smiller@crowninformationservices.com>
2025-03-13 19:27:53 -0500

    Finished workbook 4.2

--- my-code/section-9/Workbook 4.2/Tip.java
+++ my-code/section-9/Workbook 4.2/Tip.java
@@ -4,22 +4,26 @@ public class Tip {
         double bill = 53.5;
         System.out.println("Waiter: I hope you enjoyed your meal!");
         System.out.println("Thank you!");
-    
-        //Task 2: Call a function that tips the waiter.
+
+        // Task 2: Call a function that tips the waiter.
+        tipWaiter(bill);
     }
 
-
-
-    
-    //Task 1: Make a function here. See the doc comment for details.  
+    // Task 1: Make a function here. See the doc comment for details.
 
     /**
      * Function name: tipTheWaiter - calculates a tip and prints it.
+     * 
      * @param bill (double).
      * 
-     * Inside the function:
-     *  1. Calculates a tip that's 15 percent of bill.
-     *  2. Prints: Your service was wonderful! Please, accept this tip: $<tip>
+     *             Inside the function:
+     *             1. Calculates a tip that's 15 percent of bill.
+     *             2. Prints: Your service was wonderful! Please, accept this tip:
+     *             $<tip>
      */
-
+    public static  double tipWaiter(double bill) {
+        double tip = bill * 1.15;
+        System.out.println("Your service was wonderful! Please, accept this tip: $" + tip);
+        return tip;
+    }
 }
