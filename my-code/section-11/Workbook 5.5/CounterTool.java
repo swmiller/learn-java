c78db7265e46ede6d5eadcffaf7e37da714b4d8b
Stephen Miller <smiller@crowninformationservices.com>
2025-03-14 11:09:02 -0500

    Finsihed workbook 5.5

--- my-code/section-11/Workbook 5.5/CounterTool.java
+++ my-code/section-11/Workbook 5.5/CounterTool.java
@@ -3,11 +3,27 @@ import java.util.Scanner;
 public class CounterTool {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         System.out.println("I hear you like to count by threes\n");
         System.out.println("Jimmy: It depends.");
         System.out.println("Oh, ok...");
 
-       // See detailed instructions on Learn the Part.
+        // See detailed instructions on Learn the Part.
+        System.out.print("Pick a number to count by: ");
+        int countBy = scan.nextInt();
+        scan.nextLine();
+
+        System.out.print("Pick a number to start counting from: ");
+        int start = scan.nextInt();
+        scan.nextLine();
+
+        System.out.print("Pick a number to count to: ");
+        int end = scan.nextInt();
+        scan.nextLine();
+
+        for (int i = start; i <= end; i += countBy) {
+            System.out.print(i + " ");
+        }
 
         scan.close();
     }
