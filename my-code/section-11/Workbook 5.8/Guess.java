01f90064a921160f2da3a3c1fb0fe4b7d86a9cc1
Stephen Miller <smiller@crowninformationservices.com>
2025-03-14 11:22:51 -0500

    Finished workbook 5.8

--- my-code/section-11/Workbook 5.8/Guess.java
+++ my-code/section-11/Workbook 5.8/Guess.java
@@ -1,14 +1,27 @@
 import java.util.Scanner;
 
-public class Guess  {
+public class Guess {
     public static void main(String[] args) {
 
-       System.out.print("I chose a number between 1 and 5. Try to guess it: ");
-        
-       Scanner scan = new Scanner(System.in);
-       
-       //See Learn the Part for detailed instructions.
-        
+        int computerNumber = (int) (Math.random() * 5) + 1;
+
+        System.out.print("I chose a number between 1 and 5. Try to guess it: ");
+
+        Scanner scan = new Scanner(System.in);
+
+        // See Learn the Part for detailed instructions.
+        while (true) {
+            int guess = scan.nextInt();
+            scan.nextLine();
+
+            if (guess == computerNumber) {
+                System.out.println("You guessed it!");
+                break;
+            } else {
+                System.out.print("Try again: ");
+            }
+        }
+
         scan.close();
     }
 
