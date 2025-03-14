ca93fcbf8fd88879b29a7a280092f59e15822974
Stephen Miller <smiller@crowninformationservices.com>
2025-03-14 11:33:47 -0500

    Finished workbook 5.9

--- my-code/section-11/Workbook 5.9/SignIn.java
+++ my-code/section-11/Workbook 5.9/SignIn.java
@@ -4,31 +4,46 @@ public class SignIn {
     public static void main(String[] args) {
         String username = "Samantha";
         String password = "Java <3";
-        
-        /* Task 1 
-            1. Pick up a username and password from the user.
-       */
+
+        /*
+         * Task 1
+         * 1. Pick up a username and password from the user.
+         */
         Scanner scan = new Scanner(System.in);
         System.out.println("\nWelcome to Javagram! Sign in below\n");
         System.out.print("- Username: ");
-        //pick up username
+        // pick up username
+        String loginUsername = scan.nextLine();
+
         System.out.print("- Password: ");
-        //pick up password
+        // pick up password
+        String loginPassword = scan.nextLine();
 
         // see Learn the Part for the remaining instructions.
-        
-        
-        /* will be useful for task 2
-        
+        while (!loginUsername.equals(username) || !loginPassword.equals(password)) {
             System.out.println("\nIncorrect, please try again!\n");
-            System.out.print("- Username: ");
-            usernameEntry = scan.nextLine();
-            System.out.print("- Password: ");
-            passwordEntry = scan.nextLine();
 
-        
-        */
+            System.out.print("- Username: ");
+            loginUsername = scan.nextLine();
+
+            System.out.print("- Password: ");
+            loginPassword = scan.nextLine();
+        }
+        /*
+         * will be useful for task 2
+         * 
+         * System.out.println("\nIncorrect, please try again!\n");
+         * System.out.print("- Username: ");
+         * usernameEntry = scan.nextLine();
+         * System.out.print("- Password: ");
+         * passwordEntry = scan.nextLine();
+         * 
+         * 
+         */
+
+        System.out.println("\nSign in successful. Welcome, " + username + "!");
+
         scan.close();
-        
+
     }
 }
