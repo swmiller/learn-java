9cf7fc54e5c2f8cc0832685c4cdd2f149b9aa464
Stephen Miller <smiller@crowninformationservices.com>
2025-03-16 15:54:42 -0500

    Finished workbook 6.9

--- my-code/section-13/Workbook 6.9/Tax.java
+++ my-code/section-13/Workbook 6.9/Tax.java
@@ -2,11 +2,14 @@ import java.util.Arrays;
 
 public class Tax {
     public static void main(String[] args) {
-        double[] price = {1.99, 2.99, 3.99, 4.99};
+        double[] price = { 1.99, 2.99, 3.99, 4.99 };
+        double[] tax = new double[price.length];
 
-        // See instructions on Learn the Part (Workbook 6.9)
-        System.out.println("The original prices are: <original prices>");
-        System.out.println("The prices after tax are: <after tax prices>");
+        for (int i = 0; i < price.length; i++) {
+            tax[i] = price[i] * 1.13;
+        }
 
+        System.out.println("The original prices are: " + Arrays.toString(price));
+        System.out.println("The prices after tax are: " + Arrays.toString(tax));
     }
 }
