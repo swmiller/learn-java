5dbd1c1931452d63823f41f1e70a0a2137ffc001
Stephen Miller <smiller@crowninformationservices.com>
2025-03-16 15:50:12 -0500

    Finished workbook 6.8

--- my-code/section-13/Workbook 6.8/Receipt.java
+++ my-code/section-13/Workbook 6.8/Receipt.java
@@ -1,11 +1,12 @@
 public class Receipt {
     public static void main(String[] args) {
-
-
         System.out.println("Here's your receipt:\n");
-        
-        // See instructions on Learn the Part (Workbook 6.8)
-        
-        System.out.println("\t<apple i >: $<price i >"); // to be used in for loop.
+
+        String[] items = { "Gala", "Granny Smith", "Macintosh" };
+        Double[] prices = { 1.99, 1.49, 1.29 };
+
+        for (int i = 0; i < items.length; i++) {
+            System.out.println("\t" + items[i] + ": $" + prices[i]);
+        }
     }
 }
