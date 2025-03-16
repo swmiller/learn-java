a7b1486cb3b9e75a6031a7adfd333a12a515763e
Stephen Miller <smiller@crowninformationservices.com>
2025-03-16 15:43:59 -0500

    Finished workbook 6.6

--- my-code/section-13/Workbook 6.6/HighScore.java
+++ my-code/section-13/Workbook 6.6/HighScore.java
@@ -1,27 +1,32 @@
 public class HighScore {
     public static void main(String[] args) {
-        int[] scores = {randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber()};
-        
+        int[] scores = { randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(), randomNumber(),
+                randomNumber(), randomNumber(), randomNumber(), randomNumber() };
+
         int highScore = 0;
-        System.out.print("Here are the scores: ");    
+        System.out.print("Here are the scores: ");
         for (int i = 0; i < scores.length; i++) {
             System.out.print(scores[i] + " ");
             if (scores[i] > highScore) {
                 highScore = scores[i];
             }
         }
-        
-       
+
         System.out.println("\n\nThe highest score is: " + highScore + ". Impressive!");
-        
-        // TODO : See instructions on Learn the Part (Workbook 6.6)
-        System.out.println("It's the gentleman in seat: <index>. Give that man a cookie!");
+
+        int seat = 0;
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] == highScore) {
+                seat = i;
+            }
+        }
+        System.out.println("It's the gentleman in seat: " + seat + ". Give that man a cookie!");
 
     }
-    
+
     public static int randomNumber() {
         double randomDouble = Math.random() * 50000;
-        int randomInt = (int)randomDouble;
+        int randomInt = (int) randomDouble;
         return randomInt;
     }
 }
