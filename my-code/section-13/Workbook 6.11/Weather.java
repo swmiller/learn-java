3f0f59135f3eedfdbfa272771c8c06f16fde6827
Stephen Miller <smiller@crowninformationservices.com>
2025-03-16 16:06:14 -0500

    Finished workbook 6.11

--- my-code/section-13/Workbook 6.11/Weather.java
+++ my-code/section-13/Workbook 6.11/Weather.java
@@ -1,11 +1,20 @@
+import java.util.Arrays;
+
 public class Weather {
     public static void main(String[] args) {
-        
-        // The instructions for this workbook are on Learn the Part (Workbook 6.11)
-        double[] celsius = {12.5, 14.5, 17.0, 21.0, 23.0, 18.5, 20.0};
-        double[] fahrenheit;
-        
 
+        // The instructions for this workbook are on Learn the Part (Workbook 6.11)
+        double[] celsius = { 12.5, 14.5, 17.0, 21.0, 23.0, 18.5, 20.0 };
+        double[] fahrenheit = celciusToFahrenheit(celsius);
+        System.out.println(Arrays.toString(fahrenheit));
     }
 
+    private static double[] celciusToFahrenheit(double[] celsius) {
+        double[] fahrenheit = new double[celsius.length];
+        for (int i = 0; i < celsius.length; i++) {
+            fahrenheit[i] = celsius[i] * 9 / 5 + 32;
+        }
+
+        return fahrenheit;
+    }
 }
