c3de57a7161c9b61c5e44f5e620e65546d2845f9
Stephen Miller <smiller@crowninformationservices.com>
2025-03-18 12:45:20 -0500

    Finished workbook 6.14

--- my-code/section-13/Workbook 6.14/RandomNumbers.java
+++ my-code/section-13/Workbook 6.14/RandomNumbers.java
@@ -1,7 +1,35 @@
 public class RandomNumbers {
     public static void main(String[] args) {
-        
-        // The instructions for this workbook are on Learn the Part (Workbook 6.14)
+        int[][] array = new int[100][10];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = randomNumber();
+            }
+        }
+
+        print2DArray(array);
+
+    }
+
+    private static int randomNumber() {
+        double randomNumber = Math.random() * 100;
+        return (int) randomNumber;
+    }
+
+    /**
+     * Prints a 2D array
+     * 
+     * @param array
+     */
+    private static void print2DArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+
+            System.out.println();
+        }
     }
 
 }
