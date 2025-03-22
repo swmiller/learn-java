1189b345d8b560d3a05c91a48545b1286561d232
Stephen Miller <smiller@crowninformationservices.com>
2025-03-21 22:10:46 -0500

    Finished workbook 7.3

--- my-code/section-14/Workbook 7.3/Person.java
+++ my-code/section-14/Workbook 7.3/Person.java
@@ -1,10 +1,10 @@
 public class Person {
-    
-    String name;
-    String nationality;
-    String dateOfBirth;
-    String[] passport;
-    int seatNumber;
+
+    private String name;
+    private String nationality;
+    private String dateOfBirth;
+    private String[] passport;
+    private int seatNumber;
 
     public Person(String name, String nationality, String dateOfBirth, int seatNumber) {
         this.name = name;
@@ -12,5 +12,48 @@ public class Person {
         this.dateOfBirth = dateOfBirth;
         this.seatNumber = seatNumber;
     }
- 
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String[] getPassport() {
+        return passport;
+    }
+
+    public void setPassport(String[] passport) {
+        this.passport = passport;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
 }
