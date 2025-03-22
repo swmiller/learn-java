368216c080bd930c909bc7b9af1265bbda8f53da
Stephen Miller <smiller@crowninformationservices.com>
2025-03-21 22:06:29 -0500

    Finished workbook 7.2

--- my-code/section-14/Workbook 7.2/Main.java
+++ my-code/section-14/Workbook 7.2/Main.java
@@ -1,23 +1,23 @@
-import java.util.Arrays;
- 
 public class Main {
-  
-    public static void main(String[] args) {
-        Person person = new Person();
- 
-        person.name = "Rayan Slim";
-        person.nationality = "Canadian";
-        person.dateOfBirth = "01/01/1111";
-        person.passport = new String[]{person.name, person.nationality, person.dateOfBirth};
-        person.seatNumber = 5;
-        
-        System.out.println(person.name);
-        System.out.println(person.nationality);
-        System.out.println(person.dateOfBirth);
-        System.out.println(Arrays.toString(person.passport));
-        System.out.println(person.seatNumber);
 
+    public static void main(String[] args) {
+        Person person = new Person("Rayan Slim", "Canadian", "01/01/1111",
+                new String[] { "Rayan Slim", "Canadian", "01/01/1111" }, 5);
+
+        // person.name = "Rayan Slim";
+        // person.nationality = "Canadian";
+        // person.dateOfBirth = "01/01/1111";
+        // person.passport = new String[]{person.name, person.nationality,
+        // person.dateOfBirth};
+        // person.seatNumber = 5;
+
+        // System.out.println(person.name);
+        // System.out.println(person.nationality);
+        // System.out.println(person.dateOfBirth);
+        // System.out.println(Arrays.toString(person.passport));
+        // System.out.println(person.seatNumber);
+
+        System.out.println("Name: " + person.getName() + "\n" + "Nationality: " + person.getNationality() + "\n"
+                + "Date of Birth: " + person.getDateOfBirth() + "\n" + "Seat Number: " + person.getSeatNumber() + "\n");
     }
-  
-  
 }
--- my-code/section-14/Workbook 7.2/Person.java
+++ my-code/section-14/Workbook 7.2/Person.java
@@ -1,9 +1,60 @@
 public class Person {
-    
+
     String name;
     String nationality;
     String dateOfBirth;
     String[] passport;
     int seatNumber;
- 
+
+    public Person(String name, String nationality, String dateOfBirth, String[] passport, int seatNumber) {
+        this.name = name;
+        this.nationality = nationality;
+        this.dateOfBirth = dateOfBirth;
+        this.passport = passport;
+        this.seatNumber = seatNumber;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String[] getPassport() {
+        return passport;
+    }
+
+    public void setPassport(String[] passport) {
+        this.passport = passport;
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
 }
