dc499f61ca3fea6cd437e8726e517af6be5cbc31
Stephen Miller <smiller@crowninformationservices.com>
2025-03-26 15:44:34 -0500

    Finished workbook 11.1

--- my-code/section-24/Workbook/starter/Main.java
+++ my-code/section-24/Workbook/starter/Main.java
@@ -1,24 +1,24 @@
+import constants.Position;
+
 public class Main {
 
     public static void main(String[] args) {
-        
         Team bulls = new Team("Chicago Bulls");
-        bulls.setPlayer("SHOOTING_GUARD", "Michael Jordan");
-        bulls.setPlayer("SMALL_FORWARD", "Scottie Pippen");
-        bulls.setPlayer("POWER_FORWARD", "Dennis Rodman");
-        bulls.setPlayer("CENTER", "Bill Wennington");
-        bulls.setPlayer("POINT_GUARD", "Randy Brown");
+        bulls.setPlayer(Position.SHOOTING_GUARD, "Michael Jordan");
+        bulls.setPlayer(Position.SMALL_FORWARD, "Scottie Pippen");
+        bulls.setPlayer(Position.POWER_FORWARD, "Dennis Rodman");
+        bulls.setPlayer(Position.CENTER, "Bill Wennington");
+        bulls.setPlayer(Position.POINT_GUARD, "Randy Brown");
 
         Team pistons = new Team("Detroit Pistons");
-        pistons.setPlayer("SHOOTING_GUARD", "Joe Dumars");
-        pistons.setPlayer("SMALL_FORWARD", "Grant Hill");
-        pistons.setPlayer("POWER_FORWARD", "Otis Thorpe");
-        pistons.setPlayer("CENTER", "William Bedford");
-        pistons.setPlayer("POINT_GUARD", "Isiah Thomas");
+        pistons.setPlayer(Position.SHOOTING_GUARD, "Joe Dumars");
+        pistons.setPlayer(Position.SMALL_FORWARD, "Grant Hill");
+        pistons.setPlayer(Position.POWER_FORWARD, "Otis Thorpe");
+        pistons.setPlayer(Position.CENTER, "William Bedford");
+        pistons.setPlayer(Position.POINT_GUARD, "Isiah Thomas");
 
         Game game = new Game("Etihad Stadium");
         game.begin(bulls, pistons);
         System.out.println(game.details());
     }
-
 }
