5efbf3ea121b464bdf4edcb535a0b046a84983b6
Stephen Miller <smiller@crowninformationservices.com>
2025-03-26 10:04:55 -0500

    Part 2

--- my-code/section-20/Extra Practice/Part 1/Main.java
+++ my-code/section-20/Extra Practice/Part 1/Main.java
@@ -1,22 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         Magazine magazine1 = new Magazine("Magazine 1", "Publisher 1", 1, 2020);
-        System.out.println(magazine1.getTitle());
+        Magazine magazine2 = new Magazine("Magazine 2", "Publisher 2", 2, 2021);
 
-        // Uncomment the following line to test invalid input for setTitle method
-        // magazine1.setTitle("");
+        MagazineLibrary library = new MagazineLibrary();
 
-        // Uncomment the following line to test invalid input for setPublisher method
-        // magazine1.setPublisher("");
+        // Test the addMagazine method
+        library.addMagazine(magazine1);
+        library.addMagazine(magazine2);
 
-        // Uncomment the following line to test invalid input for setIssueNumber method
-        // magazine1.setIssueNumber(-1);
+        // Test the getMagazine method
+        Magazine retrievedMagazine = library.getMagazine(0);
+        System.out.println(retrievedMagazine.getTitle());
 
-        // Uncomment the following line to test invalid input for setPublicationYear
-        // method
-        // magazine1.setPublicationYear(0);
+        // Test the setMagazine method
+        Magazine newMagazine = new Magazine("Magazine 3", "Publisher 3", 3, 2022);
+        library.setMagazine(newMagazine, 0);
 
-        // Uncomment the following line to test invalid input for the constructor
-        // Magazine magazine2 = new Magazine("", "Publisher 2", 2, 2020);
+        // Verify that the magazine was updated
+        retrievedMagazine = library.getMagazine(0);
+        System.out.println(retrievedMagazine.getTitle());
     }
 }
