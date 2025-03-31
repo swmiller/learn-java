730a4992aee640d4bff8599036ae615085408eac
Stephen Miller <smiller@crowninformationservices.com>
2025-03-31 11:08:34 -0500

    Finished workbook 13.7

--- my-code/section-28/Workbooks/Workbook 13.7/starter/Main.java
+++ my-code/section-28/Workbooks/Workbook 13.7/starter/Main.java
@@ -1,10 +1,16 @@
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Path path = Paths.get("emails.txt");
-        
+        Stream<String> lines = Files.lines(path);
+        ;
+        lines
+                .filter(x -> !x.startsWith("Spam"))
+                .forEach(System.out::println);
     }
 }
