69fd7520b35d6c2686f4e2014d3f91542def071d
Stephen Miller <smiller@crowninformationservices.com>
2025-03-31 11:02:47 -0500

    Finished workbook 13.6

--- my-code/section-28/Workbooks/Workbook 13.6/starter/Main.java
+++ my-code/section-28/Workbooks/Workbook 13.6/starter/Main.java
@@ -6,26 +6,29 @@ public class Main {
         List<String> names = Arrays.asList("Alice", "Bob", "Charlie", "Dave");
 
         System.out.println("\n**************************************");
-        // names.stream()
-            
-   
+        names.stream()
+                .map(x -> "Hello, " + x)
+                .forEach(System.out::println);
+
         System.out.println("\n**************************************");
-        
+
         List<String> usernames = Arrays.asList("sparklingunicorn", "galactic_goddess", "neon_ninja", "purplepixiedust");
-     
-        // String username =  usernames.stream();
-     
-        // System.out.println("Found you: " + username);
+
+        // String username = usernames.stream();
+        String username = usernames.stream()
+                .filter(u -> u.equals("neon_ninja"))
+                .findFirst()
+                .orElse(null);
+        System.out.println("Found you: " + username);
 
         System.out.println("\n**************************************");
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
 
         // long numEven = numbers.stream()
-        
-        // System.out.println("There are " + numEven + " even numbers in this list");
-        
-    }   
-    
+        long numEven = numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .count();
+        System.out.println("There are " + numEven + " even numbers in this list");
+    }
+
 }
-
-
