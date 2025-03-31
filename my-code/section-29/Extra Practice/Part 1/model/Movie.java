fd84d9871359771eb646921ca165e09707b81497
Stephen Miller <smiller@crowninformationservices.com>
2025-03-31 12:00:46 -0500

    Finished extra practice

--- my-code/section-29/Extra Practice/Part 1/model/Movie.java
+++ my-code/section-29/Extra Practice/Part 1/model/Movie.java
@@ -2,4 +2,54 @@ package model;
 
 public class Movie {
 
+    private String title;
+    private String director;
+    private String genre;
+    private int year;
+    private double rating;
+
+    public Movie(String title, String director, String genre, int year, double rating) {
+        this.title = title;
+        this.director = director;
+        this.genre = genre;
+        this.year = year;
+        this.rating = rating;
+    }
+
+    public Movie(Movie source) {
+        if (source == null) {
+            throw new IllegalArgumentException("Source movie cannot be null");
+        }
+
+        this.title = source.title;
+        this.director = source.director;
+        this.genre = source.genre;
+        this.year = source.year;
+        this.rating = source.rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    @Override
+    public String toString() {
+        return title + "\t" + director + "\t" + genre + "\t" + year + "\t" + rating;
+    }
 }
--- my-code/section-29/Extra Practice/Part 1/model/MovieStore.java
+++ my-code/section-29/Extra Practice/Part 1/model/MovieStore.java
@@ -1,5 +1,42 @@
 package model;
 
-public class MovieStore {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MovieStore {
+    private List<Movie> movies;
+
+    public MovieStore() {
+        this.movies = new ArrayList<>();
+    }
+
+    public void addMovie(Movie movie) {
+        if (movie == null) {
+            throw new IllegalArgumentException("Movie cannot be null");
+        }
+        movies.add(new Movie(movie));
+    }
+
+    public List<Movie> filterByGenre(String genre) {
+        if (genre == null || genre.isEmpty()) {
+            throw new IllegalArgumentException("Genre cannot be null or empty");
+        }
+
+        return movies.stream()
+                .filter(movie -> movie.getGenre().equalsIgnoreCase(genre))
+                .toList();
+    }
+
+    public List<Movie> sortByReleaseYear() {
+        return movies.stream()
+                .sorted((m1, m2) -> Integer.compare(m1.getYear(), m2.getYear()))
+                .toList();
+    }
+
+    public List<Movie> getTopRatedMovies(int n) {
+        return movies.stream()
+                .sorted((m1, m2) -> Double.compare(m2.getRating(), m1.getRating()))
+                .limit(n)
+                .toList();
+    }
 }
